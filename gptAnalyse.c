4f0abeb9e2fd008ec704167d2e2cca389060ce48
jonasruhland69 <jonator@gmx.at>
2020-05-13 11:33:54 +0200

    program finished

--- gptAnalyse.c
+++ gptAnalyse.c
@@ -44,63 +44,75 @@ static FILE *fp=NULL;
 
 		  BOOL gpt_process() {
               // fp
-              int length=0;
+              int length = 0;
 
               puts("\nDEBUG::gpt_process\n");
-              fseek(fp,512,SEEK_CUR);
+              fseek(fp, 512, SEEK_CUR);
 
               //Output Signatur
               printf("\nSignatur:\n");
-              length=8;
+              length = 8;
               char *signatur = readBytes(0, length);
               printf("Dezimal:\n");
-              for(int i = 0; i<length;i++)
-                printf("%d ", signatur[i]);
+              for (int i = 0; i < length; i++)
+                  printf("%d ", signatur[i]);
               printf("\nHex:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("0x%02x ", signatur[i]);
 
               //Output Header-Größe
               printf("\n\nHeader-Größe:\n");
-              length=4;
-              strcpy(signatur,readBytes(4, length));
+              length = 4;
+              strcpy(signatur, readBytes(4, length));
               printf("Dezimal:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("%d ", signatur[i]);
               printf("\nHex:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("0x%02x ", signatur[i]);
 
               //Output Backup LBA
               printf("\n\nBackup LBA:\n");
-              length=8;
-              strcpy(signatur,readBytes(16, length));
+              length = 8;
+              strcpy(signatur, readBytes(16, length));
               printf("Dezimal:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("%d ", signatur[i]);
               printf("\nHex:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("0x%02x ", signatur[i]);
 
               //Output Anzahl der Partitionseinträge
               printf("\n\nAnzahl der Partitionseintraege:\n");
-              length=4;
-              strcpy(signatur,readBytes(40, length));
+              length = 4;
+              strcpy(signatur, readBytes(40, length));
               printf("Dezimal:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("%d ", signatur[i]);
               printf("\nHex:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("0x%02x ", signatur[i]);
 
               //Output Groesse eines Partitionseintrags
               printf("\n\nGröße eines Partitionseintrags:\n");
-              length=4;
-              strcpy(signatur,readBytes(0, length));
+              length = 4;
+              strcpy(signatur, readBytes(0, length));
               printf("Dezimal:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("%d ", signatur[i]);
               printf("\nHex:\n");
-              for(int i = 0; i<length;i++)
+              for (int i = 0; i < length; i++)
                   printf("0x%02x ", signatur[i]);
+
+
+              //Sprung zum 513. Byte
+              fseek(fp, 513L, SEEK_SET);
+              for (int i = 0; i < 32; i++) {
+                  printf("\n\nLBA %d", i);
+                  length = 16;
+                  strcpy(signatur, readBytes(0, length));
+                  printf("\nHex:\n");
+                  for (int i = 0; i < length; i++)
+                      printf("0x%02x ", signatur[i]);
+              }
           }
